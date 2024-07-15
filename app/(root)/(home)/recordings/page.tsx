4b930e17128f9aafbf57dd531ce0c0f4f864d37a
Ayan564 <ayanmaity564@gmail.com>
2024-07-15 13:41:41 +0530

    create other function

--- app/(root)/(home)/recordings/page.tsx
+++ app/(root)/(home)/recordings/page.tsx
@@ -1,9 +1,13 @@
-import React from "react";
-const Recordings = () => {
+import CallList from "@/components/CallList";
+
+const PreviousPage = () => {
   return (
-    <section className="flex size-full flex-col gap-10 text-white ">
-      <h1 className="text-3xl font-bold "> Recordings</h1>
+    <section className="flex size-full flex-col gap-10 text-white">
+      <h1 className="text-3xl font-bold">Recordings</h1>
+
+      <CallList type="recordings" />
     </section>
   );
 };
-export default Recordings;
+
+export default PreviousPage;
--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,5 +1,13 @@
-import React, { ReactNode } from "react";
-const RootLayout = ({ children }: { children: ReactNode }) => {
-  return <main>{children}</main>;
+import { ReactNode } from "react";
+
+import StreamVideoProvider from "@/providers/StreamClientProvider";
+
+const RootLayout = ({ children }: Readonly<{ children: ReactNode }>) => {
+  return (
+    <main>
+      <StreamVideoProvider>{children}</StreamVideoProvider>
+    </main>
+  );
 };
+
 export default RootLayout;
